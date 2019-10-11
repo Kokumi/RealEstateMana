900d7af9fd7b0d7b61e4cfee409d4462973d997a
Debruyckere <fldebruyck@numericable.fr>
2019-10-11 17:59:09 +0200

    add String line(Values) create EditActivity

---
+++ app/src/main/java/com/openclassrooms/realestatemanager/controller/EditActivity.kt
@@ -0,0 +1,13 @@
+package com.openclassrooms.realestatemanager.controller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.openclassrooms.realestatemanager.R
+
+class EditActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit)
+    }
+}
